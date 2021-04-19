ed4edee49f67b956e33466470bc833ccd4711a2f
Santiago Libonati <santiago.libonati@gmail.com>
2021-04-19 15:47:13 -0300

    Creadas funciones columnas y filas

--- prototipo.py
+++ prototipo.py
@@ -20,6 +20,21 @@ def contenidoFila(nro_fila, tablero):
     fila = tablero[6-nro_fila]
     return fila
 
+def columnas(tablero):
+    tableroVacio = []
+    for nro_columna in range(1,7):
+        columna = contenidoColumna(nro_columna, tablero)
+        tableroVacio.append(columna)
+    return tableroVacio
+
+def filas(tablero):
+    tableroVacio = []
+    for nro_fila in range(1,7):
+        fila = contenidoFila(nro_fila, tablero)
+        tableroVacio.append(fila)
+    return tableroVacio
+
+
 def soltarFichaEnColumna(ficha, columna, tablero):
     for fila in range(6, 0, -1):
         if tablero[fila-1][columna-1] == 0:
@@ -62,3 +77,11 @@ print(resultado.format(buscarColumna, contenidoColumna(buscarColumna, tablero)))
 buscarFila = 1
 resultado = "\nEl contenido de la fila {} es {}"
 print(resultado.format(buscarFila, contenidoFila(buscarFila, tablero)))
+
+tableroColumnas = columnas(tablero)
+print("\nEl contenido de las columnas es")
+print(tableroColumnas)
+
+tableroFilas = filas(tablero)
+print("\El contenido de las filas es")
+print(tableroFilas)
